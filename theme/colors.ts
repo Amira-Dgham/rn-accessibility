7c5ead37fb803aa1d40bd56fa8ab882e6e3ce54f
dghama <adgham@mobelite.fr>
2025-06-12 00:05:50 +0100

    Add theme colors and initial structure for images and index files

--- theme/colors.ts
+++ theme/colors.ts
@@ -2,9 +2,11 @@
  * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
-
 const tintColorLight = '#0a7ea4';
 const tintColorDark = '#fff';
+
+const purpleLight = '#9b59b6';
+const purpleDark = '#d7aefb';
 
 export const Colors = {
   light: {
@@ -14,6 +16,9 @@ export const Colors = {
     icon: '#687076',
     tabIconDefault: '#687076',
     tabIconSelected: tintColorLight,
+    purple: purpleLight,
+    purpleBackground: '#f3e8fc',
+    purpleText: '#4a0072',
   },
   dark: {
     text: '#ECEDEE',
@@ -22,5 +27,8 @@ export const Colors = {
     icon: '#9BA1A6',
     tabIconDefault: '#9BA1A6',
     tabIconSelected: tintColorDark,
+    purple: purpleDark,
+    purpleBackground: '#2a1a37',
+    purpleText: '#e1bfff',
   },
 };
